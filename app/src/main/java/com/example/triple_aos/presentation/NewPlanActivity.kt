f2ff4ee2d7d2b625bb593234adb626bab85091de
YuBeen-Park <park_ubin@naver.com>
2022-11-23 20:22:41 +0900

    [feat] 일정생성 뷰 UI 구현 - 헤더 - 탭 레이아웃 - 일정 생성 첫 뷰(버튼) - 1일차 일정

---
+++ app/src/main/java/com/example/triple_aos/presentation/NewPlanActivity.kt
@@ -0,0 +1,15 @@
+package com.example.triple_aos.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil.setContentView
+import com.example.triple_aos.R
+import com.example.triple_aos.base.BindingActivity
+import com.example.triple_aos.databinding.ActivityNewPlanBinding
+
+class NewPlanActivity : BindingActivity <ActivityNewPlanBinding>(R.layout.activity_new_plan){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
