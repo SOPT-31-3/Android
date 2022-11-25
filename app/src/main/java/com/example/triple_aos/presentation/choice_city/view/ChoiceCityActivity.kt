c0fe65f0e2076b31053c3fd643db84d441123a8d
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2022-11-25 17:55:52 +0900

    [feat] 도시선택 뷰 구현 완료

--- app/src/main/java/com/example/triple_aos/presentation/choice_city/view/ChoiceCityActivity.kt
+++ app/src/main/java/com/example/triple_aos/presentation/choice_city/view/ChoiceCityActivity.kt
@@ -3,23 +3,49 @@ package com.example.triple_aos.presentation.choice_city.view
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView.LayoutManager
 import com.example.triple_aos.R
 import com.example.triple_aos.base.BindingActivity
 import com.example.triple_aos.databinding.ActivityChoiceCityBinding
+import com.example.triple_aos.presentation.choice_city.adapter.ChoiceCityAdapter
+import com.example.triple_aos.presentation.choice_city.data.CityAdd
+import com.example.triple_aos.presentation.choice_city.data.CityInfo
 import com.example.triple_aos.presentation.choice_city.viewmodel.ChoiceCityViewModel
 import com.example.triple_aos.util.ItemOffsetDecoration
 
 class ChoiceCityActivity :
     BindingActivity<ActivityChoiceCityBinding>(R.layout.activity_choice_city) {
     private val viewModel: ChoiceCityViewModel by viewModels()
+    private val choiceRecycleData by lazy {
+        listOf(
+            CityInfo("뉴욕", R.drawable.city_item_sample),
+            CityInfo("뉴욕", R.drawable.city_item_sample),
+            CityInfo("뉴욕", R.drawable.city_item_sample),
+            CityInfo("뉴욕", R.drawable.city_item_sample),
+            CityInfo("뉴욕", R.drawable.city_item_sample),
+            CityInfo("뉴욕", R.drawable.city_item_sample),
+            CityInfo("뉴욕", R.drawable.city_item_sample),
+            CityInfo("뉴욕", R.drawable.city_item_sample),
+            CityInfo("뉴욕", R.drawable.city_item_sample),
+            CityInfo("뉴욕", R.drawable.city_item_sample),
+            CityInfo("뉴욕", R.drawable.city_item_sample),
+            CityAdd("도시 검색", R.drawable.ic_search)
+        )
+    }
+    private val adapter by lazy {
+        ChoiceCityAdapter(this).apply {
+            submitList(
+                choiceRecycleData
+            )
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.vm = viewModel
         binding.lifecycleOwner = this
 
         binding.choiceGridRv.layoutManager = GridLayoutManager(this, 3)
-        binding.choiceGridRv.addItemDecoration( ItemOffsetDecoration(this,8))
+        binding.choiceGridRv.addItemDecoration(ItemOffsetDecoration(this,8))
         initAdapter()
     }
 
