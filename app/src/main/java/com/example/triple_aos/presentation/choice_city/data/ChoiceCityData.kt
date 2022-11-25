1837342e2d5ba02f919f4ada6d052e73d31a31cb
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2022-11-25 17:14:17 +0900

    Revert "[feat] ChoiceCityData 추가"
    This reverts commit c8a8adc9686739566b0cf99eaebefc754976921f.

--- app/src/main/java/com/example/triple_aos/presentation/choice_city/data/ChoiceCityData.kt
+++
@@ -1,9 +0,0 @@
-package com.example.triple_aos.presentation.choice_city.data
-
-sealed class ChoiceCityData
-
-const val CITY_INFO_TYPE = 0
-const val CITY_ADD_TYPE = 1
-
-data class CityInfo(val name: String, val image: Int):ChoiceCityData()
-data class CityAdd(val desc: String, val image: Int):ChoiceCityData()
